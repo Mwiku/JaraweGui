7eee3cd297d07fe60c89c3f6ddf25b94690a42ef
Mwiku <mwizak@gmail.com>
2024-04-01 22:36:18 +0200

    Add components, add bootstrap for styling

---
+++ src/app/elections/elections.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mk-elections',
+  standalone: true,
+  imports: [],
+  templateUrl: './elections.component.html',
+  styleUrl: './elections.component.scss'
+})
+export class ElectionsComponent {
+
+}
---
+++ src/app/publications/publications.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mk-publications',
+  standalone: true,
+  imports: [],
+  templateUrl: './publications.component.html',
+  styleUrl: './publications.component.scss'
+})
+export class PublicationsComponent {
+
+}
